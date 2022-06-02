8ad4c9013978d73e7982dacea3d9ed4fa9ab5bd7
jama <abdiasisjama1989@gmail.com>
2022-06-02 22:59:34 +0300

    class model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+class StudentImage(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='online_university/images')
